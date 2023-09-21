1fbbed07369cbb7605fb645c8e1ea52c991de7bb
eduardolopesx02 <143762383+eduardolopesx02@users.noreply.github.com>
2023-09-21 19:31:13 -0300

    Delete Atividade directory

--- Atividade/Questão 3/script.js
+++
@@ -1,9 +0,0 @@
-function concatenarDados() {
-    const nome = document.getElementById('nome').value;
-    const estadoCivil = document.getElementById('estadoCivil').value;
-    const faixaEtaria = document.getElementById('faixaEtaria').value;
-    const mensagem = document.getElementById('mensagem');
-
-    const resultado = `Nome: ${nome}, Estado Civil: ${estadoCivil}, Faixa Etária: ${faixaEtaria}`;
-    mensagem.textContent = resultado;
-}
